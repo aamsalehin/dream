6359f5338353a85e0fcc53b17104f7896903dcc1
aamsalehin <65489361+aamsalehin@users.noreply.github.com>
2020-06-26 21:46:21 +0600

    merge

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,7 @@
 
 $(document).ready(function(){
   
-   $(".onclc").focus(function(){
-    $(this).css({"background-color": "transparent", "border":"2px #c89f2d solid"});
-  }); 
+ 
     
     
     
